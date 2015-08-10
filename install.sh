df44c5784190e7ee3757b9d573eb44251822d294
Raghav Kumar Gautam <raghav@apache.org>
2015-08-10 15:19:30 -0700

    install emacs

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-sudo apt-get install -y vim curl python-software-properties
+sudo apt-get install -y vim curl emacs python-software-properties
 sudo apt-get update
 sudo apt-get -y install mysql-server
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
